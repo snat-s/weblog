ad0eef3f206b6b80bf591b2415d3411fa5b15f74
snat-s <santpedrozadiaz@gmail.com>
2024-02-16 18:19:52 -0600

    post fuyu & dtrocr

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 module.exports = function (eleventyConfig) {
   
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("**/*.png");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPlugin(pluginRss);
 
