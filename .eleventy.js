c801c7e4f1b765b920d1c1e33e178f5ea1bf0218
snat-s <santpedrozadiaz@gmail.com>
2024-08-30 11:18:35 -0600

    new blog post about little helper

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
   
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("**/*.png");
+  eleventyConfig.addPassthroughCopy("**/*.gif");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPlugin(pluginRss);
 
